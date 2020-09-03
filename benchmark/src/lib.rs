cb59487b336f0cde951d67671f773128afb98db3
Leonard Kriese <leonard.kriese@turbinekreuzberg.com>
2020-09-04 00:53:29 +0100

    first benchmark - string concatenation

---
+++ benchmark/src/lib.rs
@@ -0,0 +1,45 @@
+mod utils;
+use wasm_bindgen::prelude::*;
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+
+#[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    // The `console.log` is quite polymorphic, so we can bind it with multiple
+    // signatures. Note that we need to use `js_name` to ensure we always call
+    // `log` in JS.
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u32(a: u32);
+
+    // Multiple arguments too!
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_many(a: &str, b: &str);
+}
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, er!");
+}
+
+#[wasm_bindgen]
+pub fn iterate_array(length: i32, default_value: String) {
+    let mut my_string: String = "".to_string();
+    for i in 0..100000000 {
+        my_string.push_str("a");
+    }
+
+
+}
